5994bd417d2812cc01267468ba6ee05a4589eee0
Ben Hattem <benghattem@gmail.com>
2022-11-06 08:39:59 +0100

    added Powershell menu for creating projects

--- README.md
+++ README.md
@@ -4,8 +4,14 @@
 This repository contains [Cookiecutter](https://cookiecutter.readthedocs.io/) templates for Python development projects (on Windows). All templates use a combination of make with [pip-tools](https://pypi.org/project/pip-tools/) to manage a virtual environment:
   * __windows_qt__: basic Qt / PySide2 application (using VSCode and QT Designer)
   * __windows_standalone_exe__: standalone (Windows) executable (with an example how the combination of `click`, `logging` (both to file and console) and `pyinstaller` can be used)
-  * __windows_vsc__: basic VSCode template
+  * __windows_vsc__: basic VSCode template.
 
+You can use the PowerShell script `cookiecutter.ps1` to create new projects from the templates:
+  - Copy `cookiecutter.ps1` to your main project directory (projects will be created as sub-directories).
+  - Adapt configuration in `cookiecutter.ps1`:
+    * `COOKIECUTTER`: location of the `cookiecutter.exe` executable
+    * `TEMPLATE_DIRS`: templates should be sub-directories of this folder
+  - Run `cookiecutter.ps1` and use the menu to select the template
 
 ## Visual Studio Code
 In case you want to use Visual Studio Code for Python development, the following set of extensions might be handy (in alphabetical order):
@@ -19,7 +25,6 @@ In case you want to use Visual Studio Code for Python development, the following
   * reStructuredText
   * SQLite
   * Visual Studio IntelliCode
-
 
 ## make
 Templates 'abuse' `make` for managing the virtual environment and/or building an executable. In case you need `make` for Windows: use [GnuWin](http://gnuwin32.sourceforge.net/). You can either install the entire set or just [make](http://gnuwin32.sourceforge.net/packages/make.htm). In fact, you only need these files (unpack from the zips and put them somewhere in your path):
---
+++ cookiecutter.ps1
@@ -0,0 +1,250 @@
+# Use cookiecutter to create a project from a template
+#
+# V1.0   2022-03-03 initial version
+
+#
+# CONFIGURATION
+#
+$config = @{
+    'COOKIECUTTER' = 'cookiecutter.exe'
+    'TEMPLATE_DIRS' = '.\python_templates'
+    'TEMPLATES' = [ordered]@{}
+}
+
+# end of configuration
+
+
+#
+# USERINTERFACE
+#
+function Set-HostColors {
+    $background = 'DarkMagenta'
+    $Host.UI.RawUI.BackgroundColor = $background
+    $Host.UI.RawUI.ForegroundColor = 'DarkYellow'
+    $Host.PrivateData.ErrorForegroundColor = 'Red'
+    $Host.PrivateData.ErrorBackgroundColor = $background
+    $Host.PrivateData.WarningForegroundColor = 'Yellow'
+    $Host.PrivateData.WarningBackgroundColor = $background
+    $Host.PrivateData.DebugForegroundColor = 'Yellow'
+    $Host.PrivateData.DebugBackgroundColor = $background
+    $Host.PrivateData.VerboseForegroundColor = 'Yellow'
+    $Host.PrivateData.VerboseBackgroundColor = $background
+    $Host.PrivateData.ProgressForegroundColor = 'Yellow'
+    $Host.PrivateData.ProgressBackgroundColor = $background
+}
+
+
+function Get-MenuSelection {
+    # based on: https://www.koupi.io/post/creating-a-powershell-console-menu
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [String[]]$MenuItems,
+        [String]$MenuPrompt,
+        [switch]$ChooseByFirstLetter,
+        [switch]$ExitWithX
+    )
+
+    # get mapping from (lowercase) first key to item
+    $firstCharToPos = @{}
+    for($i=0; $i -lt $MenuItems.Count; $i++) {
+        if ($MenuItems[$i] -notmatch '^\s*$') {
+            $firstCharToPos[$MenuItems[$i].substring(0,1).tolower()] = $i
+        }
+    }
+
+    # store initial cursor position
+    $cursorPosition = $host.UI.RawUI.CursorPosition
+    $pos = 0 # current item selection
+
+    function Write-Menu {
+        param (
+            [int]$selectedItemIndex
+        )
+        # reset the cursor position and write prompt
+        $Host.UI.RawUI.CursorPosition = $cursorPosition
+        Write-Host $MenuPrompt -ForegroundColor Green
+
+        # write the menu lines
+        $maxLineLength = ($MenuItems | Measure-Object -Property Length -Maximum).Maximum + 4
+        for ($i=0; $i -lt $MenuItems.Count; $i++) {
+            $line = "    $($MenuItems[$i])" + (' ' * ($maxLineLength - $MenuItems[$i].Length))
+            if ($selectedItemIndex -eq $i) {
+                Write-Host $line -ForegroundColor Blue -BackgroundColor Gray
+            } else {
+                Write-Host $line
+            }
+        }
+    }
+
+    Write-Menu -selectedItemIndex $pos
+    $virtKey = $null
+    $exitWithoutChoice = $false
+    while ($virtKey -ne 13) {
+        # handle keypress
+        $keyPress = $host.ui.rawui.readkey('NoEcho,IncludeKeyDown')
+        $virtKey = $keyPress.virtualkeycode
+        if ((37, 38) -contains $virtKey -and $pos -gt 0) {  # cursor left / cursor up
+            $pos--
+        } elseif ((39, 40) -contains $virtKey -and $pos -lt $MenuItems.Count-1) {  # cursor right / cursor down
+            $pos++
+        } elseif ($virtKey -eq 36) {  # Home
+            $pos = 0
+        } elseif ($virtKey -eq 35) {  # End
+            $pos = $MenuItems.Count - 1
+        } elseif ($ExitWithX -and ([string]$keyPress.character).tolower() -eq 'x'){
+            $exitWithoutChoice = $true
+            $virtKey = 13
+        } elseif ($ChooseByFirstLetter -and $firstCharToPos.keys -contains ([string]$keyPress.character).tolower()) {
+            $pos = $firstCharToPos[([string]$keyPress.character).tolower()]
+            $virtKey = 13
+        }
+        Write-Menu -selectedItemIndex $pos
+    }
+
+    if ($exitWithoutChoice) {
+        return $null
+    } else {
+        return $MenuItems[$pos]
+    }
+}
+
+
+function Read-AnyKey {
+    Write-Host -NoNewline 'Press any key to continue...'
+    # ignore keys like Alt, Ctrl, Shift, Tab, etc...
+    $ignore = 9, 16, 17, 18, 20, 91, 92, 93, 144, 145, 166, 167, 168, 169, `
+        170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183
+    $keyPress = $null
+    while ($keyPress.VirtualKeyCode -eq $null -or $ignore -contains $keyPress.VirtualKeyCode) {
+        $keyPress = $Host.UI.RawUI.ReadKey('NoEcho, IncludeKeyDown')
+    }
+    Write-Host
+}
+
+
+function Write-Underlined {
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string]$Text,
+        [string]$UnderlineChar='-'
+    )
+    Write-Host ($Text.substring(0, 1).toupper() + $Text.substring(1))
+    $underline = $UnderlineChar * $Text.length
+    Write-Host $underline
+}
+
+
+#
+# FUNCTIONS
+#
+function Get-Templates {
+    param(
+        [ValidateNotNullOrEmpty()]
+        [HashTable]$Config
+    )
+    $templates = @{}
+    foreach ($templateDir in Get-ChildItem -Directory -Exclude .git -Path $Config.TEMPLATE_DIRS) {
+        $template = Join-Path -Path $templateDir -ChildPath cookiecutter.json
+        if (Test-Path -Path $template) {
+            $templateInfo = Get-Content $template -Raw | ConvertFrom-Json
+            $templates.Add($templateInfo.repo_name, $templateDir)
+        }
+    }
+    $templates.getEnumerator() | Sort-Object -property key | ForEach-Object {
+        $Config.TEMPLATES.Add($_.key, $_.value)
+    }
+    return $Config
+}
+
+
+function Invoke-Cmd {
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string]$CmdExe,
+        [ValidateNotNullOrEmpty()]
+        [string]$Msg,
+        [Parameter(ValueFromRemainingArguments)]
+        [string[]]$Arguments
+    )
+    Write-Underlined $Msg '-'
+    & $CmdExe $Arguments
+    if ($?) {
+        Write-Host ''
+    } else {
+        Throw "could not $Msg"
+    }
+}
+
+#
+# MENU AND ACTION HANDLERS
+#
+function Invoke-Cookiecutter {
+    param(
+        [ValidateNotNullOrEmpty()]
+        [string]$Template,
+        [ValidateNotNullOrEmpty()]
+        [HashTable]$Config
+    )
+    clear
+    Invoke-Cmd -CmdExe $Config.COOKIECUTTER -Msg "creating project for template: $Template" "$($Config.TEMPLATES[$Template])"
+    Read-AnyKey
+}
+
+
+function New-MainMenu {
+    param(
+        [ValidateNotNullOrEmpty()]
+        [HashTable]$Config
+    )
+    $menu = [ordered]@{}
+    foreach ($template in $Config.TEMPLATES.keys) {
+        $menu.Add($template, $template)
+    }
+    $menu.Add('Exit (do nothing)', 'EXIT')
+    return $menu
+}
+
+
+function Show-MainMenu {
+    param(
+        [ValidateNotNullOrEmpty()]
+        [System.Collections.Specialized.OrderedDictionary]$Menu,
+        [ValidateNotNullOrEmpty()]
+        [HashTable]$Config
+    )
+    $exitNotChosen = $true
+    while ($exitNotChosen) {
+        clear
+        $choice = Get-MenuSelection -MenuItems $Menu.Keys -MenuPrompt 'Create project from cookiecutter template:'
+        if ($Menu[$choice] -eq 'EXIT') {
+            $exitNotChosen = $false
+        } else {
+            Invoke-Cookiecutter -Template $Menu[$choice] -Config $Config
+        }
+    }
+}
+
+#
+# MAIN
+#
+try {
+    $ErrorActionPreference = 'Continue'
+
+    $config = Get-Templates -Config $config
+    $menu = New-MainMenu -Config $config
+    Show-MainMenu -Menu $menu -Config $config
+} catch {
+    Write-Host "`nGot an unhandled error (execution will be stopped):"
+    Write-Host -ForegroundColor Red $_
+    Write-Host
+    Write-Host -ForegroundColor Red $_.InvocationInfo.PositionMessage
+    Write-Host
+    Write-Host -ForegroundColor Red 'StackTrace:'
+    Write-Host -ForegroundColor Red $_.ScriptStackTrace
+    Write-Host
+    Read-AnyKey
+    exit 1
+}
+exit 0
