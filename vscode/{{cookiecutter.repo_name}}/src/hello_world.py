66fe47f62e314f499436d84d5640c4c040ee7362
Ben Hattem <benghattem@gmail.com>
2024-12-02 17:33:58 +0100

    add an example with portable shebang

---
+++ vscode/{{cookiecutter.repo_name}}/src/hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Hello World"""
+
+
+def main():
+    """Hello World."""
+    print("Hello World from {{cookiecutter.repo_name}}!")
+
+
+if __name__ == "__main__":
+    main()
