39a15cd32a6ec8a4283132bc14c54ce11dcf2f2a
Ben Hattem <benghattem@gmail.com>
2025-03-21 13:44:14 +0100

    update TUI to latest version

--- cookiecutter.ps1
+++ cookiecutter.ps1
@@ -1,130 +1,173 @@
 # Use cookiecutter to create a project from a template
 #
-# V1.0   2022-03-03 initial version
-# V1.1   2022-03-18 add 'x' (exit) option to menu
+# V1.0    2022-03-03  initial version
+# V1.1    2022-03-18  add 'x' (exit) option to menu
+# V1.1.1  2025-03-21  update TUI with newer version
 
 #
 # CONFIGURATION
 #
 $config = @{
-    'COOKIECUTTER' = 'cookiecutter.exe'
+    'COOKIECUTTER'  = 'cookiecutter.exe'
     'TEMPLATE_DIRS' = '.\python_templates'
-    'TEMPLATES' = [ordered]@{}
+    'TEMPLATES'     = [ordered]@{}
 }
 
 # end of configuration
 
 
 #
-# USERINTERFACE
+# TEXT USER INTERFACE (TUI)
 #
 function Set-HostColors {
-    $background = 'DarkMagenta'
-    $Host.UI.RawUI.BackgroundColor = $background
-    $Host.UI.RawUI.ForegroundColor = 'DarkYellow'
-    $Host.PrivateData.ErrorForegroundColor = 'Red'
-    $Host.PrivateData.ErrorBackgroundColor = $background
-    $Host.PrivateData.WarningForegroundColor = 'Yellow'
-    $Host.PrivateData.WarningBackgroundColor = $background
-    $Host.PrivateData.DebugForegroundColor = 'Yellow'
-    $Host.PrivateData.DebugBackgroundColor = $background
-    $Host.PrivateData.VerboseForegroundColor = 'Yellow'
-    $Host.PrivateData.VerboseBackgroundColor = $background
-    $Host.PrivateData.ProgressForegroundColor = 'Yellow'
-    $Host.PrivateData.ProgressBackgroundColor = $background
+    $colors = @{
+        'background' = 'Black'
+        'foreground' = 'Gray'
+        'error'      = 'Red'
+        'highlight'  = 'Yellow'
+    }
+    $Host.UI.RawUI.BackgroundColor = $colors.background
+    $Host.UI.RawUI.ForegroundColor = $colors.foreground
+    $Host.PrivateData.ErrorForegroundColor = $colors.error
+    $Host.PrivateData.ErrorBackgroundColor = $colors.background
+    $Host.PrivateData.WarningForegroundColor = $colors.highlight
+    $Host.PrivateData.WarningBackgroundColor = $colors.background
+    $Host.PrivateData.DebugForegroundColor = $colors.highlight
+    $Host.PrivateData.DebugBackgroundColor = $colors.background
+    $Host.PrivateData.VerboseForegroundColor = $colors.highlight
+    $Host.PrivateData.VerboseBackgroundColor = $colors.background
+    $Host.PrivateData.ProgressForegroundColor = $colors.highlight
+    $Host.PrivateData.ProgressBackgroundColor = $colors.background
+    Clear-Host
 }
 
 
 function Get-MenuSelection {
-    # based on: https://www.koupi.io/post/creating-a-powershell-console-menu
-    [CmdletBinding()]
-    [OutputType([string])]
+    # initially based on: https://www.koupi.io/post/creating-a-powershell-console-menu
     param (
+        [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [String[]]$MenuItems,
-        [String]$MenuPrompt,
         [switch]$ChooseByFirstLetter,
         [switch]$ExitWithX,
-        [int]$Selected = 0
+        [switch]$MultiSelect,
+        [String]$MenuPrompt = 'Please choose:',
+        [int]$Selected = 0,
+        [String]$MenuLineStart = '  ',
+        [String]$MultiSelMarker = 'x'
     )
 
-    # get mapping from (lowercase) first key to item
-    $firstCharToPos = @{}
-    for($i=0; $i -lt $MenuItems.Count; $i++) {
-        if ($MenuItems[$i] -notmatch '^\s*$') {
-            $firstCharToPos[$MenuItems[$i].substring(0,1).tolower()] = $i
-        }
-    }
-
-    # store initial cursor position
-    $cursorPosition = $host.UI.RawUI.CursorPosition
-    $pos = $Selected # current item selection
-    if ($pos -lt 0) {
-        $pos = 0
-    } elseif ($pos -ge $MenuItems.Count) {
-        $pos = $MenuItems.Count - 1
-    }
-
     function Write-Menu {
-        param (
-            [int]$selectedItemIndex
-        )
         # reset the cursor position and write prompt
-        $Host.UI.RawUI.CursorPosition = $cursorPosition
+        $Host.UI.RawUI.CursorPosition = $initialCursorPosition
         Write-Host $MenuPrompt -ForegroundColor Green
-
-        # write the menu lines
-        $maxLineLength = ($MenuItems | Measure-Object -Property Length -Maximum).Maximum + 4
-        for ($i=0; $i -lt $MenuItems.Count; $i++) {
-            $line = "    $($MenuItems[$i])" + (' ' * ($maxLineLength - $MenuItems[$i].Length))
-            if ($selectedItemIndex -eq $i) {
-                Write-Host $line -ForegroundColor Blue -BackgroundColor Gray
+        # show the menu
+        for ($i = 0; $i -lt $Menu.Count; $i++) {
+            if ($pos -eq $i) {
+                Write-Host $Menu[$i] -ForegroundColor Blue -BackgroundColor Gray
             } else {
-                Write-Host $line
+                Write-Host $Menu[$i]
             }
         }
     }
 
-    Write-Menu -selectedItemIndex $pos
-    $virtKey = $null
-    $exitWithoutChoice = $false
-    while ($virtKey -ne 13) {
-        # handle keypress
-        $keyPress = $host.ui.rawui.readkey('NoEcho,IncludeKeyDown')
-        $virtKey = $keyPress.virtualkeycode
-        if ((37, 38) -contains $virtKey -and $pos -gt 0) {  # cursor left / cursor up
-            $pos--
-        } elseif ((39, 40) -contains $virtKey -and $pos -lt $MenuItems.Count-1) {  # cursor right / cursor down
-            $pos++
-        } elseif ($virtKey -eq 36) {  # Home
-            $pos = 0
-        } elseif ($virtKey -eq 35) {  # End
-            $pos = $MenuItems.Count - 1
-        } elseif ($ExitWithX -and ([string]$keyPress.character).tolower() -eq 'x'){
-            $exitWithoutChoice = $true
-            $virtKey = 13
-        } elseif ($ChooseByFirstLetter -and $firstCharToPos.keys -contains ([string]$keyPress.character).tolower()) {
-            $pos = $firstCharToPos[([string]$keyPress.character).tolower()]
-            $virtKey = 13
+    # -ChooseByFirstLetter: get mapping from first letter to item
+    $firstCharToPos = @{}
+    foreach ($i in 0..($MenuItems.Count - 1)) {
+        $item = $MenuItems[$i]
+        if (-not [string]::IsNullOrWhiteSpace($item)) {
+            $firstCharToPos[$item.Substring(0, 1).ToLower()] = $i
         }
-        Write-Menu -selectedItemIndex $pos
     }
 
-    if ($exitWithoutChoice) {
-        return $null
-    } else {
-        return $MenuItems[$pos]
+    # -Selected: check / set current item selection
+    $pos = [Math]::Max(0, [Math]::Min($Selected, $MenuItems.Count - 1))
+
+    # prepare menu for display
+    $maxLineLength = ($MenuItems | Measure-Object -Property Length -Maximum).Maximum + 4
+    if ($MultiSelect) {
+        if ([string]::IsNullOrWhiteSpace($MultiSelMarker)) {
+            throw 'Parameter -MultiSelMarker cannot be an empty string'
+        }
+        $multiSelectPos = $MenuLineStart.Length + 1
+        $multiSelNotMarked = ' ' * $MultiSelMarker.Length
     }
+    $Menu = @($MenuItems | ForEach-Object {
+            $menuLine = $MenuLineStart
+            if ($MultiSelect) {
+                $menuLine += "[$multiSelNotMarked] "
+            }
+            $menuLine += "$_ " + (' ' * ($maxLineLength - $_.Length))
+            $menuLine
+        })
+
+    # start menu
+    $exitWithoutChoice = $false
+    $virtKey = $null
+    $initialCursorPosition = $host.UI.RawUI.CursorPosition
+
+    while ($virtKey -ne 13) {
+        Write-Menu
+        $keyPress = $host.ui.rawui.readkey('NoEcho,IncludeKeyDown')
+        $virtKey = $keyPress.virtualkeycode
+
+        # handle key press
+        switch ($virtKey) {
+            { $_ -in 37..38 } { if ($pos -gt 0) { $pos-- } }  # Left/Up
+            { $_ -in 39..40 } { if ($pos -lt $MenuItems.Count - 1) { $pos++ } }  # Right/Down
+            36 { $pos = 0 }  # Home
+            35 { $pos = $MenuItems.Count - 1 }  # End
+            { $ExitWithX -and ([string]$keyPress.character).ToLower() -eq 'x' } {
+                $exitWithoutChoice = $true
+                $virtKey = 13  # simulate <enter>
+            }
+            { $ChooseByFirstLetter -and $firstCharToPos.ContainsKey(([string]$keyPress.character).ToLower()) } {
+                $pos = $firstCharToPos[([string]$keyPress.character).ToLower()]
+                $virtKey = if ($MultiSelect) { 32 } else { 13 }  # simulate <space> or <enter>
+            }
+        }
+
+        # -MultiSelect (when enabled): triggered by <space>
+        if ($MultiSelect -and $virtKey -eq 32) {
+            $currentSelect = $Menu[$pos].Substring($multiSelectPos, $MultiSelMarker.Length)
+            $newSelect = if ($currentSelect -eq $MultiSelMarker) { $multiSelNotMarked } else { $MultiSelMarker }
+            $Menu[$pos] = $Menu[$pos].Substring(0, $multiSelectPos) + $newSelect + $Menu[$pos].Substring($multiSelectPos + $MultiSelMarker.Length)
+        }
+    }
+
+    if (-not $exitWithoutChoice) {
+        # a choice was made: evaluate
+        if ($MultiSelect) {
+            # get all selected entries when in -MultiSelect mode
+            $selection = for ($i = 0; $i -lt $Menu.Count; $i++) {
+                if ($Menu[$i].substring($multiSelectPos, $MultiSelMarker.Length) -eq $MultiSelMarker) {
+                    $MenuItems[$i]
+                }
+            }
+            # return when at least one item is selected (otherwise return $null)
+            if ($selection.Count -gt 0) {
+                return $selection
+            }
+        } else {
+            return $MenuItems[$pos]
+        }
+    }
+    return $null
 }
 
 
 function Read-AnyKey {
-    Write-Host -NoNewline 'Press any key to continue...'
+    param (
+        [string]$Prompt = 'Press any key to continue...'
+    )
+    Write-Host -NoNewline $Prompt
     # ignore keys like Alt, Ctrl, Shift, Tab, etc...
-    $ignore = 9, 16, 17, 18, 20, 91, 92, 93, 144, 145, 166, 167, 168, 169, `
-        170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183
+    $ignoreKeys = @(
+        9, 16, 17, 18, 20, 91, 92, 93, 144, 145, 166, 167, 168, 169, 170,
+        171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183
+    )
     $keyPress = $null
-    while ($null -eq $keyPress.VirtualKeyCode -or $ignore -contains $keyPress.VirtualKeyCode) {
+    while ($null -eq $keyPress.VirtualKeyCode -or $ignoreKeys -contains $keyPress.VirtualKeyCode) {
         $keyPress = $Host.UI.RawUI.ReadKey('NoEcho, IncludeKeyDown')
     }
     Write-Host
@@ -133,12 +176,13 @@ function Read-AnyKey {
 
 function Write-Underlined {
     param (
+        [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$Text,
-        [string]$UnderlineChar='-'
+        [string]$UnderlineChar = '-'
     )
-    Write-Host ($Text.substring(0, 1).toupper() + $Text.substring(1))
-    $underline = $UnderlineChar * $Text.length
+    Write-Host $Text
+    $underline = $UnderlineChar * $Text.Length
     Write-Host $underline
 }
 
@@ -159,7 +203,7 @@ function Get-Templates {
             $templates.Add($templateInfo.repo_name, $templateDir)
         }
     }
-    $templates.getEnumerator() | Sort-Object -property key | ForEach-Object {
+    $templates.getEnumerator() | Sort-Object -Property key | ForEach-Object {
         $Config.TEMPLATES.Add($_.key, $_.value)
     }
     return $Config
@@ -194,7 +238,7 @@ function Invoke-Cookiecutter {
         [ValidateNotNullOrEmpty()]
         [HashTable]$Config
     )
-    clear
+    Clear-Host
     Invoke-Cmd -CmdExe $Config.COOKIECUTTER -Msg "creating project for template: $Template" "$($Config.TEMPLATES[$Template])"
     Read-AnyKey
 }
@@ -223,7 +267,7 @@ function Show-MainMenu {
     )
     $exitNotChosen = $true
     while ($exitNotChosen) {
-        clear
+        Clear-Host
         $choice = Get-MenuSelection -MenuItems $Menu.Keys -MenuPrompt 'Create project from cookiecutter template (''x'' to exit):' -ExitWithX
         if (($null -eq $choice) -or ($Menu[$choice] -eq 'EXIT')) {
             $exitNotChosen = $false
